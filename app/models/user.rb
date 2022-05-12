fe5e64b1f8750e6bc4bc2194d777a14bfc0243d9
kotarokatsunuma <katsunuma1113@gmail.com>
2022-05-12 21:56:11 +0900

    URL直打ち対策

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true, length: {maximum: 6} 
+  validates :email, presence: true, uniqueness: true, length: {maximum: 50} 
+
+  has_many :books
 end
