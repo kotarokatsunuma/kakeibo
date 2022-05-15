0af3cba92e597440a233f7ed5dee88bd3302c3b6
kotarokatsunuma <katsunuma1113@gmail.com>
2022-05-15 15:41:55 +0900

    userモデルの単体テスト

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true, length: {maximum: 6} 
-  validates :email, presence: true, uniqueness: true, length: {maximum: 50} 
+  validates :email, presence: true, length: {maximum: 50} 
 
   has_many :books
 end
